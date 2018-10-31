0ba6037c65405d64472cf3cd777627d6edf0e3a5
shyguy00013 <42643797+shyguy00013@users.noreply.github.com>
2018-10-31 11:58:02 +0800

    Fixing version name
    Fixing version name

--- src/superpainter/SuperPainter.java
+++ src/superpainter/SuperPainter.java
@@ -11,7 +11,7 @@ public class SuperPainter {
     //自動日期版本設定
     static boolean Auto_date_State = false;
     //設定標題及手動版本號
-    static String  APPVERSION_static = "20181013 build 0507";
+    static String  APPVERSION_static = "20181030 build 0507";
     static String Title = "SuperPrinter";
     //
     
