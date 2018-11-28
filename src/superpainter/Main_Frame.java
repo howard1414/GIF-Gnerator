0cf1afd1581d242c0a46c9ac4d517b081a36b03c
shyguy00013 <42643797+shyguy00013@users.noreply.github.com>
2018-11-28 13:19:37 +0800

    20181128 build 1318
    -Update Resizeable Drawing space

--- src/superpainter/Main_Frame.java
+++ src/superpainter/Main_Frame.java
@@ -12,16 +12,22 @@ import javax.swing.JPanel;
 */
 public class Main_Frame extends Frame {
     public JPanel Panel_Main = new JPanel();
+    public JPanel Panel_size;
+    private JButton size_btn;
+    private int Drawing_space_x=500,Drawing_space_y=500;
     Main_Drawing_space Main_Drawing_space ;
     Messgebar  Messgebar ;
+    
      Main_Frame(String APPVERSION,String Title) {
-         Main_Drawing_space = new Main_Drawing_space(this);
-         Messgebar = new Messgebar();
+        Main_Drawing_space = new Main_Drawing_space(this);
+        
+        Messgebar = new Messgebar();
         this.setSize(700,700);
         this.setLocation(100, 20);
         this.add(Panel_Main);
         Panel_Main.setLayout(new BorderLayout());
         Panel_Main.setBackground(new Color( 50 , 50  ,50));
+        //add_Panel_Main();
         this.setTitle(Title + " - " + APPVERSION);
         Add_Object(this);
         this.setVisible(true);
@@ -31,8 +37,12 @@ public class Main_Frame extends Frame {
                 {
                         System.exit(0);
                 }
+                
             }
         );
+        
+
+        
 }
 
     void Add_Object(Main_Frame MF){
@@ -43,7 +53,32 @@ public class Main_Frame extends Frame {
     }
     
     void add_Panel_Main(){
-         Panel_Main.add(Main_Drawing_space);
+        Panel_size = new JPanel();   
+        Panel_size.setLayout(null);
+        Main_Drawing_space.setBounds(0,0, Drawing_space_x, Drawing_space_y);
+        size_btn = new JButton();
+        size_btn.setBounds(Drawing_space_x,Drawing_space_y, 15, 15);
+        Panel_size.add(Main_Drawing_space);
+        Panel_size.add(size_btn);
+        Panel_Main.add(Panel_size);
+        
+        size_btn.addMouseListener(
+                new MouseAdapter()
+                {
+                    public void mouseReleased(MouseEvent e)
+                    {   
+                        Drawing_space_x = (int)MouseInfo.getPointerInfo().getLocation().x- Panel_Main.getLocationOnScreen().x - 100;
+                        Drawing_space_y = (int)MouseInfo.getPointerInfo().getLocation().y- Panel_Main.getLocationOnScreen().y;
+                        if(Drawing_space_x <0){Drawing_space_x=0;}
+                        if(Drawing_space_y <0){Drawing_space_y=0;}
+                        size_btn.setBounds( Drawing_space_x, Drawing_space_y, 15, 15);
+                        Main_Drawing_space.setBounds(0,0,  Drawing_space_x,  Drawing_space_y);
+                       
+                    }    
+                }    
+        );
+        
+
     }
     
 }
