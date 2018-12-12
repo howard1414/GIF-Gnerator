656e2e62177a7793f62a16cdda9f0ee243aceea8
live9224192241 <44428007+live9224192241@users.noreply.github.com>
2018-12-12 21:28:45 +0800

    Revert "Revert "Merge branch 'Jderlier' of https://github.com/howard1414/SuperPainter into Jderlier""
    This reverts commit 00e69d0d36afed0bf704c807e1805355522716cb.

--- src/superpainter/ToolbarBTN.java
+++ src/superpainter/ToolbarBTN.java
@@ -16,6 +16,7 @@ import javax.swing.JPanel;
 public class ToolbarBTN extends JPanel{
     JButton saveBTN,loadBTN,optBTN,lineBTN,pencilBTN,circleBTN,closeBTN,chang3d,recoveryBTN,rectBTN,addnewPG,palette,line_width;
     JButton pencilall,fileall,patternall,backup;
+    Output output;
     Button_Status BS;
     Boolean toolbarVisible=true;
     public final static JPanel Panel_Button = new JPanel();
@@ -189,7 +190,13 @@ public class ToolbarBTN extends JPanel{
                     public void mouseClicked(MouseEvent e)
                     {    
                          MF.Messgebar.setLB("輸出");
+                         if(output==null){
+                         output = new Output(MF);
+                         }else{
+                         JOptionPane.showMessageDialog(null,"輸出視窗已開啟");
+                         }
                     }
+                         
                 }
         );
         pencilBTN.addMouseListener(
