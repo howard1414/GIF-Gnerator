268f803dbc7899c545daa2ea9abba7a4821d73e0
live9224192241 <44428007+live9224192241@users.noreply.github.com>
2019-01-02 22:26:57 +0800

    Update SuperPainter.java

--- src/superpainter/SuperPainter.java
+++ src/superpainter/SuperPainter.java
@@ -11,7 +11,7 @@ public class SuperPainter {
     //自動日期版本設定
     static boolean Auto_date_State = false;
     //設定標題及手動版本號
-    static String  APPVERSION_static = "20190102 build 2105";
+    static String  APPVERSION_static = "20190102 build 2226";
     static String Title = "SuperPrinter";
     //
     
