64731396f979acd9aece86b2875dd0c8da14e9d9
shyguy00013 <42643797+shyguy00013@users.noreply.github.com>
2019-01-09 13:57:46 +0800

    20190109 build 1357
    -Remove unnecessary blocks

--- src/superpainter/GifSequenceWriter.java
+++ src/superpainter/GifSequenceWriter.java
@@ -30,17 +30,7 @@ public class GifSequenceWriter {
   protected ImageWriteParam imageWriteParam;
   protected IIOMetadata imageMetaData;
   
-  /**
-   * Creates a new GifSequenceWriter
-   * 
-   * @param outputStream the ImageOutputStream to be written to
-   * @param imageType one of the imageTypes specified in BufferedImage
-   * @param timeBetweenFramesMS the time between frames in miliseconds
-   * @param loopContinuously wether the gif should loop repeatedly
-   * @throws IIOException if no gif ImageWriters are found
-   *
-   * @author Elliot Kroo (elliot[at]kroo[dot]net)
-   */
+
   public GifSequenceWriter(
       ImageOutputStream outputStream,
       int imageType,
@@ -111,21 +101,12 @@ public class GifSequenceWriter {
       imageWriteParam);
   }
   
-  /**
-   * Close this GifSequenceWriter object. This does not close the underlying
-   * stream, just finishes off the GIF.
-   */
+  
   public void close() throws IOException {
     gifWriter.endWriteSequence();    
   }
 
-  /**
-   * Returns the first available GIF ImageWriter using 
-   * ImageIO.getImageWritersBySuffix("gif").
-   * 
-   * @return a GIF ImageWriter object
-   * @throws IIOException if no GIF image writers are returned
-   */
+  
   private static ImageWriter getWriter() throws IIOException {
     Iterator<ImageWriter> iter = ImageIO.getImageWritersBySuffix("gif");
     if(!iter.hasNext()) {
@@ -135,15 +116,7 @@ public class GifSequenceWriter {
     }
   }
 
-  /**
-   * Returns an existing child node, or creates and returns a new child node (if 
-   * the requested node does not exist).
-   * 
-   * @param rootNode the <tt>IIOMetadataNode</tt> to search for the child node.
-   * @param nodeName the name of the child node.
-   * 
-   * @return the child node, if found or a new node created with the given name.
-   */
+  
   private static IIOMetadataNode getNode(
       IIOMetadataNode rootNode,
       String nodeName) {
@@ -159,42 +132,5 @@ public class GifSequenceWriter {
     return(node);
   }
   
-  /**
-  public GifSequenceWriter(
-       BufferedOutputStream outputStream,
-       int imageType,
-       int timeBetweenFramesMS,
-       boolean loopContinuously) {
-   
-   */
-  /*
-  public static void main(String[] args) throws Exception {
-    if (args.length > 1) {
-      // grab the output image type from the first image in the sequence
-      BufferedImage firstImage = ImageIO.read(new File(args[0]));
-
-      // create a new BufferedOutputStream with the last argument
-      ImageOutputStream output = 
-        new FileImageOutputStream(new File(args[args.length - 1]));
-      
-      // create a gif sequence with the type of the first image, 1 second
-      // between frames, which loops continuously
-      GifSequenceWriter writer = 
-        new GifSequenceWriter(output, firstImage.getType(), 1, false);
-      
-      // write out the first image to our sequence...
-      writer.writeToSequence(firstImage);
-      for(int i=1; i<args.length-1; i++) {
-        BufferedImage nextImage = ImageIO.read(new File(args[i]));
-        writer.writeToSequence(nextImage);
-      }
-      
-      writer.close();
-      output.close();
-    } else {
-      System.out.println(
-        "Usage: java GifSequenceWriter [list of gif files] [output file]");
-    }
-  }
-  */
+  
 }
--- src/superpainter/SuperPainter.java
+++ src/superpainter/SuperPainter.java
@@ -11,7 +11,7 @@ public class SuperPainter {
     //自動日期版本設定
     static boolean Auto_date_State = false;
     //設定標題及手動版本號
-    static String  APPVERSION_static = "20190109 build 1235";
+    static String  APPVERSION_static = "20190109 build 1357";
     static String Title = "SuperPrinter";
     //
     
